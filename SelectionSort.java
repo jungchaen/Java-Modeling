283c22330c9ac3574a530fe3ac37b3a6e35808e7
jungchuen <yllowrose452@naver.com>
2022-07-15 22:38:51 +0900

    Sort

---
+++ SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+    public static int[] numbers = { 3, 15, 8, 1, 4, 5 };
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        printArray(numbers);
+        selectionSort(numbers);
+        printArray(numbers);
+    }
+}
